54e4fb75f57caab43890aaff4e0dd0d689d41061
samuelemoscatelli <sem.mosca97@libero.it>
2018-11-26 14:48:10 +0100

    correzione classe gestorecentraline

--- java/samuele/GestoreCentraline.java
+++ java/samuele/GestoreCentraline.java
@@ -1,69 +1,54 @@
+import java.util.ArrayList;
+import java.rmi.*;
+import java.rmi.server.*;
+import java.rmi.registry.*;
 
-public class GestoreCentraline {
-	private CentralinaAutomobilistica[] listaCentralineAuto;
-	private CentralinaStradale[] listaCentralineStradali;
+public class GestoreCentraline extends UnicastRemoteObject implements IGestoreCentraline {
+	private ArrayList<CentralinaAutomobilistica> listaCentralineAuto;
+	private ArrayList<CentralinaStradale> listaCentralineStradali;
 	private static GestoreCentraline istance=null;
+	
     private GestoreCentraline() {
-    	this.listaCentralineAuto=new CentralinaAutomobilistica[8000];
-    	this.listaCentralineStradali=new CentralinaStradale[100];
+    	this.listaCentralineAuto=new ArrayList<CentralinaAutomobilistica>();
+    	this.listaCentralineStradali=new ArrayList<CentralinaStradale>();
     }
+    
     public static GestoreCentraline getIstance() {
             if(istance==null)
                     istance = new GestoreCentraline();
             return istance;
     }
     public void aggiungiCentralinaAuto(CentralinaAutomobilistica centralina) {
-    	for (int i=0; i<8000; i++) {
-    		if (this.listaCentralineAuto[i]==null) {
-    			this.listaCentralineAuto[i]=centralina;
-    			break;
-    		}
-    	}
-    	if (i==8000) {
-    		System.out.println("impossibile aggiungere una nuova cetralina");
-    	}
+    	this.listaCentralineAuto.add(centralina);
     }
+    
     public void aggiungiCentralinaStradale(CentralinaStradale centralina) {
-    	int i=0;
-    	for (i=0; i<100; i++) {
-    		if (this.listaCentralineStradali[i]==null) {
-    			this.listaCentralineStradali[i]=centralina;
-    			break;
-    		}
-    	}
-    	if (i==100) {
-    		System.out.println("impossibile aggiungere una nuova cetralina");
-    	}
+    	this.listaCentralineStradali.add(centralina);
     }
+    
     public void rimuoviCentralinaAuto(int id) {
-    	int i=0;
-    	for (i=0; i<8000; i++) {
-    		if (this.listaCentralineAuto[i].getIdVeicolo()==id) {
-    			this.listaCentralineAuto[i]=null;
+    	for (CentralinaAutomobilistica var : this.listaCentralineAuto) {
+    		if (var.getIdVeicolo()==id) {
+    			this.listaCentralineAuto.remove(var);
     			break;
     		}
     	}
-    	if(i==8000) {
-    		System.out.println("non esiste nessuna centralina corrispondente all'id");
-    	}
     }
+    
     public void rimuoviCentralinaStradale(int id) {
-    	int i=0;
-    	for (i=0; i<100; i++) {
-    		if (this.listaCentralineStradali[i].getIdCentralinaStradale()==id) {
-    			this.listaCentralineStradali[i]=null;
+    	for (CentralinaStradale var : this.listaCentralineStradali) {
+    		if (var.getIdCentralinaStradale()==id) {
+    			this.listaCentralineStradali.remove(var);
     			break;
     		}
     	}
-    	if(i==100) {
-    		System.out.println("non esiste nessuna centralina corrispondente all'id");
-    	}
     }
+    
     public void segnalaDatabaseS(DatoTraffico dato) {
-    	GestoreDatabase.getIstance().aggiungiDatoTraffico(dato);
+    	GestoreDatabase.getInstance().aggiungiDatoTraffico(dato);
     }
     public void segnalaDatabaseA(StatoVeicolo dato) {
-    	GestoreDatabase.getIstance().aggiungiStatoVeicolo(dato);
+    	GestoreDatabase.getInstance().aggiungiStatoVeicolo(dato);
     }
     
     
