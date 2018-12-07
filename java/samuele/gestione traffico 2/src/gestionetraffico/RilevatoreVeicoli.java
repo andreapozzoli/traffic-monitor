d8d144570986b77a69de1f2466bba5d8d2558916
samuelemoscatelli <sem.mosca97@libero.it>
2018-12-07 18:09:41 +0100

    classi java

---
+++ java/samuele/gestione traffico 2/src/gestionetraffico/RilevatoreVeicoli.java
@@ -0,0 +1,57 @@
+package gestionetraffico;
+import java.util.*;
+
+public class RilevatoreVeicoli implements Runnable {
+	private int numeroVeicoli;
+	private RilevatoreVelocitaS rilevatoreVelocita;
+	//secondo me necessario attributo rilevatore velocità associato e lo togliamo da centralina
+	
+	public RilevatoreVeicoli () {
+		this.rilevatoreVelocita=new RilevatoreVelocitaS();
+		this.numeroVeicoli = 0;
+	}
+	
+	public RilevatoreVelocitaS getRilevatoreVelocita() {
+		return this.rilevatoreVelocita;
+	}
+
+	public int getNumeroVeicoli() {
+		this.rilevaVeicoli();
+		return this.numeroVeicoli;
+	}
+	public void setNumeroVeicoli(int numeroVeicoli) {
+		this.numeroVeicoli=numeroVeicoli;
+	}
+
+	public void reset() {
+		this.numeroVeicoli = 0;
+	}
+
+	public void rilevaVeicoli() {
+		//da fare
+		Random random = new Random();
+		int a = 0; // numero minimo
+		int b = 1; // numero massimo
+		int c = ((b-a) + 1);
+		int rilevato= random.nextInt(c) + a;
+		System.out.println("veicolo: " + rilevato);
+		if (rilevato==1) {
+			this.numeroVeicoli++;
+			this.rilevatoreVelocita.rilevaVelocita();
+		}
+	}
+	
+	//metodo run
+	public void run() {
+		while (true) {
+			rilevaVeicoli();
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ java/samuele/gestione traffico 2/src/gestionetraffico/SensoreGPSTelefono.java
@@ -0,0 +1,49 @@
+package gestionetraffico;
+import java.util.*;
+
+import jxl.*;
+import jxl.read.biff.BiffException;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.io.*;
+
+public class SensoreGPSTelefono extends SensoreGPS {
+	
+	public SensoreGPSTelefono () throws BiffException, IOException {
+		super();
+	}
+	
+	public Posizione rilevaPosizione() throws BiffException, IOException {
+		
+		double latitudine=0;		
+		double longitudine=0;
+		String via;
+		Workbook wb= Workbook.getWorkbook(new File("C://Users//semmo//Documents//eclipse//gestione traffico 2//vie3.xls"));
+		Sheet sheet = wb.getSheet(0);
+		Random random = new Random();
+		int a = 0; // numero minimo
+		int b = 543; // numero massimo
+		int c = ((b-a) + 1);
+		int miavar = random.nextInt(c) + a;
+		
+		
+		
+		Cell cella = sheet.getCell(0,miavar);
+		via = cella.getContents();
+		cella=sheet.getCell(1,miavar);
+		String lat=cella.getContents();
+		latitudine=Double.valueOf(cella.getContents());
+		cella=sheet.getCell(2,miavar);
+		longitudine=Double.valueOf(cella.getContents());
+		
+		
+		this.posizione=new Posizione(via,latitudine,longitudine);
+		return this.posizione;
+		
+	}
+	
+	
+
+}
