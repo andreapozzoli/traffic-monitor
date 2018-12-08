106869623892df37f5f20dbb28fb245cd339031e
Nicolò Pinciroli <nicolopinci.1997@gmail.com>
2018-12-08 15:00:47 +0100

    Aggiornamento prototipo app mobile

--- java/nicolo/gestione traffico/src/gestionetraffico/AppMobileGrafica.java
+++ java/nicolo/gestione traffico/src/gestionetraffico/AppMobileGrafica.java
@@ -11,63 +11,71 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
+import javax.swing.*;
 
 public class AppMobileGrafica extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
+
+	private static JTextArea areaTesto = new JTextArea();
+	private static JScrollPane areaNotifiche = new JScrollPane(areaTesto);
+	private static JPanel panel = new JPanel(new BorderLayout());
+	private static JPanel panelTop = new JPanel();
+	private static JPanel panelBottom = new JPanel();
+	
+	private static JButton segnalaCodaBtn = new JButton("Segnala coda");
+	private static JButton svuotaNotificheBtn = new JButton("Svuotare area delle notifiche");
+
 	public AppMobileGrafica() {
 		super("App mobile");
-		setSize(400, 400);
-
-		setLayout(new BorderLayout());
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setExtendedState(JFrame.MAXIMIZED_BOTH);
-		JPanel panel = new JPanel(new BorderLayout());
-		JPanel panelTop = new JPanel();
-		JPanel panelBottom = new JPanel();
-
 
 		add(panel, BorderLayout.SOUTH);
 
 		panel.add(panelTop, BorderLayout.NORTH);
 		panel.add(panelBottom, BorderLayout.SOUTH);
-
-		JButton segnalaCodaBtn = new JButton("Segnalare una coda");
-		JButton pulisciNotificheBtn = new JButton("Svuotare l'area delle notifiche");
-
-
+		
+		panelTop.add(areaNotifiche, BorderLayout.CENTER);
+		
+		/*areaNotifiche.setLineWrap(true);
+		areaNotifiche.setWrapStyleWord(true);*/
+		
 		segnalaCodaBtn.addActionListener(e -> segnalaCoda());
-		pulisciNotificheBtn.addActionListener(e -> pulisciNotifiche());
-
+		svuotaNotificheBtn.addActionListener(e -> pulisciNotifiche());
+		
 		panelBottom.add(segnalaCodaBtn);
-		panelBottom.add(pulisciNotificheBtn);
+		panelBottom.add(svuotaNotificheBtn);
+		
+		
 
+		
+		
+	}
 
-
-
+	public static void creaGUI(){
+		JFrame frame = new AppMobileGrafica();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 
 	}
-	
-	
+
+
 	public void segnalaCoda() {
 		System.out.println("Coda");
 	}
-	
-	public void pulisciNotifiche() {
-		System.out.println("Pulizia notifiche");
-	}
-	
-	public static void main(String[] args) {
-		new AppMobileGrafica().setVisible(true);
 
+	private void pulisciNotifiche() {
+		//areaNotifiche.
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				creaGUI();
+			}
+		});
 	}
 
 }
+
