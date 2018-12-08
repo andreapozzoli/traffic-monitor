e1b8bb4897b1e82004ccce28ef7f3e1f3c6c8794
Nicolò Pinciroli <nicolopinci.1997@gmail.com>
2018-12-08 15:28:25 +0100

    Aggiornamento app mobile grafica

--- java/nicolo/gestione traffico/src/gestionetraffico/AppMobileGrafica.java
+++ java/nicolo/gestione traffico/src/gestionetraffico/AppMobileGrafica.java
@@ -18,13 +18,12 @@ public class AppMobileGrafica extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-
 	private static JTextArea areaNotifiche = new JTextArea(10, 10);
 	private static JScrollPane paneNotifiche = new JScrollPane(areaNotifiche);
 	private static JPanel panel = new JPanel(new BorderLayout());
 	private static JPanel panelTop = new JPanel();
 	private static JPanel panelBottom = new JPanel();
-	
+
 	private static JButton segnalaCodaBtn = new JButton("Segnala coda");
 	private static JButton svuotaNotificheBtn = new JButton("Svuotare area delle notifiche");
 
@@ -32,44 +31,28 @@ public class AppMobileGrafica extends JFrame {
 		super("App mobile");
 
 		// da https://coderanch.com/t/341045/java/expand-JTextArea-main-panel-resized
-		
-	    paneNotifiche.setPreferredSize(new Dimension(175,150));
 
-		/*add(panel, BorderLayout.SOUTH);
+		paneNotifiche.setPreferredSize(new Dimension(175,150));
 
-		panel.add(panelTop, BorderLayout.NORTH);
-		panel.add(panelBottom, BorderLayout.SOUTH);
-		
-		
-		
+		JPanel jp = new JPanel(new BorderLayout());
+		JPanel top = new JPanel();
+		JPanel bottom = new JPanel();
+		bottom.add(segnalaCodaBtn);
+		bottom.add(svuotaNotificheBtn);
+
+		JPanel left = new JPanel();
+		JPanel right = new JPanel();
+		jp.add(top,BorderLayout.NORTH);
+		jp.add(bottom,BorderLayout.SOUTH);
+		jp.add(left,BorderLayout.WEST);
+		jp.add(right,BorderLayout.EAST);
+		jp.add(paneNotifiche,BorderLayout.CENTER);
+		getContentPane().add(jp);
+
 		segnalaCodaBtn.addActionListener(e -> segnalaCoda());
 		svuotaNotificheBtn.addActionListener(e -> pulisciNotifiche());
-		
-		panelBottom.add(segnalaCodaBtn);
-		panelBottom.add(svuotaNotificheBtn);*/
-	    
-	    JPanel jp = new JPanel(new BorderLayout());
-	    JPanel top = new JPanel();
-	    JPanel bottom = new JPanel();
-	    bottom.add(segnalaCodaBtn);
-	    bottom.add(svuotaNotificheBtn);
 
-	    JPanel left = new JPanel();
-	    JPanel right = new JPanel();
-	    jp.add(top,BorderLayout.NORTH);
-	    jp.add(bottom,BorderLayout.SOUTH);
-	    jp.add(left,BorderLayout.WEST);
-	    jp.add(right,BorderLayout.EAST);
-	    jp.add(paneNotifiche,BorderLayout.CENTER);
-	    getContentPane().add(jp);
-	    
-	    segnalaCodaBtn.addActionListener(e -> segnalaCoda());
-		svuotaNotificheBtn.addActionListener(e -> pulisciNotifiche());
-		
-		
 
-		
-		
 	}
 
 	public static void creaGUI(){
@@ -82,7 +65,7 @@ public class AppMobileGrafica extends JFrame {
 
 
 	public void segnalaCoda() {
-		System.out.println("Coda");
+		System.out.println("Funzione che segnala la coda... da fare...");
 	}
 
 	private void pulisciNotifiche() {
