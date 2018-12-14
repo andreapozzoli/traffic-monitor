0d5fd31f1d67ea2d6eee5f614ad19f6947fe3da7
andreapozzoli <pozzoliandrea97@gmail.com>
2018-12-14 18:10:18 +0100

    progetto funzionante con dettagli

--- java/nicolo/gestionetraffico/src/gestionetraffico/IGestoreApplicazioni.java
+++
@@ -1,9 +0,0 @@
-package gestionetraffico;
-import java.rmi.*;
-
-public interface IGestoreApplicazioni extends Remote{
-	public void aggiungiApplicazione(ApplicazioneMobile applicazione) throws RemoteException;
-	public void rimuoviApplicazione(int identificativo) throws RemoteException;
-	public void segnalaDatabase(NotificaApplicazione notifica) throws RemoteException;
-
-}
