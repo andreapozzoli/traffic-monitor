3ac05ef2690c140f62746f696efaaa9527b5e323
samuelemoscatelli <sem.mosca97@libero.it>
2018-12-17 12:55:09 +0100

    penultima versione eliminata

--- java/progetto penultima versione/SistemaCentrale/src/prog/IGestoreCentraline.java
+++
@@ -1,8 +0,0 @@
-package prog;
-import java.rmi.*;
-
-public interface IGestoreCentraline extends Remote{
-    public void aggiungiCentralinaStradale(int id) throws RemoteException;
-    public void rimuoviCentralinaStradale(int id) throws RemoteException; 
-    public void segnalaDatabaseS(DatoTraffico dato) throws RemoteException, NotBoundException;
-}
